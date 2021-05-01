7d2e861fbce0bf628c72241a3409279fd2b82493
YuliaKri <juliya.krivorotko@gmail.com>
2021-05-01 11:59:20 -0600

    new start structure

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -1,6 +1,7 @@
 function cors(app) {
+  console.log('cors');
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '+');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
