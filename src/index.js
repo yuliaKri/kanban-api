0a397496ea198b6f23cbbc230e409fe3ac4d3682
YuliaKri <juliya.krivorotko@gmail.com>
2021-05-01 10:01:21 -0600

    port 5000 and any port commit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const logger = require('./modules/core/logger');
 const cors = require('./modules/core/cors');
 
 const app = express();
-const PORT = 5000; //process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 logger(app);
 dbConnection();
