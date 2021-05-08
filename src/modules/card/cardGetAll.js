d08308a665cd64d405a69353545af126db3f7f55
YuliaKri <juliya.krivorotko@gmail.com>
2021-05-08 11:28:40 -0600

    cardCetAll updated

--- src/modules/card/cardGetAll.js
+++ src/modules/card/cardGetAll.js
@@ -1,5 +1,15 @@
+const Card = require('./Model');
+
 function cardGetAll(req, res) {
-  res.send('card get all');
+  Card.find()
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('Card get all error');
+    });
 }
 
 module.exports = cardGetAll;
