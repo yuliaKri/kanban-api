f5a75ff0387dc0c68b9fadf024a6fd654fb904d2
YuliaKri <juliya.krivorotko@gmail.com>
2021-05-08 11:26:52 -0600

    card creation updated

--- src/modules/card/cardCreate.js
+++ src/modules/card/cardCreate.js
@@ -1,15 +1,15 @@
-const User = require('./Model');
+const Card = require('./Model');
 
 function cardCreate(req, res) {
   console.log(req.body);
   // res.send('userRegister is here');
-  const newUser = new User({
+  const newCard = new Card({
     name: req.body.name,
     description: req.body.description,
     status: req.body.status,
     priority: req.body.priority,
   });
-  newUser.save();
+  newCard.save();
   res.status(200).json('card created');
   //console.log('new card should be created from react');
 }
